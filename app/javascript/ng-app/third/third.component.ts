d23983c39244ea94ed0556dd6bd2548026ef9473
panmai0tanu <panmai_tanu@cmu.ac.th>
2018-06-20 11:07:16 +0700

    edit from comment

--- app/javascript/ng-app/third/third.component.ts
+++ app/javascript/ng-app/third/third.component.ts
@@ -7,19 +7,5 @@ import { UserService } from '../user/user.service';
   providers: [ UserService ]
 })
 export class ThirdComponent {
-  user: any;
-  users: any[];
-  usersColumns: any[] = [
-    { prop: 'email' },
-    { prop: 'role' }
-  ];
 
-  constructor(private userService: UserService) {
-    this.userService.all().subscribe((users: any[]) => {
-      this.users = users;
-    });
-    this.userService.find(1).subscribe((user: object) => {
-      this.user = user;
-    });
-  }
 }
